282272b8f94079daa40e5b13172387d80ea0b734
Vũ Phương Trang <89772723+Selina211@users.noreply.github.com>
2021-08-30 15:27:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
+    introTitle: 'Babiii à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnIntro: ':)',
+    title: 'Có nhớ hôm nay là ngày gì khôngg 😙',
+    desc: 'thử nói ko xem == ',
+    btnYes: 'Anh có nhớ :)',
+    btnNo: 'Anh ko nhớ đâu :))',
+    question:'Thế là ngày gì :))?',
+    btnReply: 'Gửi bae <3',
+    reply: 'kỉ niệm 2 năm',
+    mess: 'Thui đùa đấy ncl chúc mừng kỉ niệm 😘 yêu anh nhiều nhiều :3',
+    messDesc: 'nhớ anh nhiều nữa. hết dịch phải đón em đi chơi nhá:<',
+    btnAccept: 'Oke bae <3',
+    messLink: 'https://www.facebook.com/phgtrang211' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
